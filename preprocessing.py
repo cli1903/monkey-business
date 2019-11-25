3718ffce91ea74f7ca9b27d882c8db8783eea374
cli1903 <47994462+cli1903@users.noreply.github.com>
2019-11-24 20:50:00 -0500

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -8,7 +8,6 @@ import numpy as np
 """
 Created on Thu Nov 21 16:18:16 2019
 
-@author: cindyli
 """
 
 def align_kin(file):
@@ -23,8 +22,14 @@ def align_kin(file):
     
     aligned = []
     spks = []
+    stacked_bins = []
+    motion_labels = []
     
     for i in range(6):
+        
+        trial_stack = []
+        trial_motion = []
+        
         start_bins = np.arange(np.min(kin_time[:,i][0]), np.max(kin_time[:,i][0]), bin_size)
         
         tk = kin[:,i][0]
@@ -32,21 +37,38 @@ def align_kin(file):
         
         spkcounts = []
         for j in range(260):
-            spkcounts.append((np.histogram(spike[0][i][0][j], start_bins)[0])[:-1])
-            
-    
+            spkcounts.append((np.histogram(spike[0][i][0][j], start_bins)[0])[1:].tolist())
+
         bintime = start_bins[2:]
         f = interp1d(kin_time[:,i][0][0], tk)
-        aligned.append(f(bintime))
+        
+        kin_move = f(bintime)
+        
+        aligned.append(kin_move)
         spks.append(spkcounts)
         
-    return aligned, spks
+        
+        for j in range(0, len(bintime), 10):
+            
+            if j + 10 >= len(bintime):
+                break
+            
+            trial_stack.append(spkcounts[:][j: j + 10])
+            trial_motion.append(kin_move[:, j + 10])
+            
+        
+        #print(trial_motion)
+        stacked_bins.append(trial_stack)
+        motion_labels.append(trial_motion)
+        
+    return stacked_bins, motion_labels
+            
+            
+    
+        
     
 
 #monkey1 = 'COS071212_MOCAP.mat'
 #monkey2 = 'GAR080710_MOCAP.mat'
     
-trait_aligned, spkcounts = align_kin('COS071212_MOCAP.mat')
-
-
-
+fr, km = align_kin('COS071212_MOCAP.mat')
