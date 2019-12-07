6312e5eb5aa8ee303b5917ab75795f9d90a480c1
cli1903 <47994462+cli1903@users.noreply.github.com>
2019-12-07 17:30:54 -0500

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -7,14 +7,14 @@ import numpy as np
 
 """
 Created on Thu Nov 21 16:18:16 2019
-
 """
 
-def align_kin(file):
+def get_data(file):
     monkey = loadmat(file)
     
+    print("loaded")
+    
     kin = monkey["KIN"]
-    #kin_lab = monkey["kin_labels"]
     kin_time = monkey["kin_times"]
     spike = monkey["spikes"]
     
@@ -22,8 +22,7 @@ def align_kin(file):
     
     aligned = []
     spks = []
-    stacked_bins = []
-    motion_labels = []
+
     
     for i in range(6):
         
@@ -32,8 +31,7 @@ def align_kin(file):
         
         start_bins = np.arange(np.min(kin_time[:,i][0]), np.max(kin_time[:,i][0]), bin_size)
         
-        tk = kin[:,i][0]
-        
+        tk = kin[:,i][0]       
         
         spkcounts = []
         for j in range(260):
@@ -47,21 +45,34 @@ def align_kin(file):
         aligned.append(kin_move)
         spks.append(spkcounts)
         
+        #print(np.array(spkcounts).shape)     
         
-        for j in range(0, len(bintime), 10):       
+        for j in range(0, len(bintime), 50):       
             if j + 10 >= len(bintime):
                 break
-            trial_stack.append(np.array(spkcounts)[:][j: j + 10])
-            trial_motion.append(kin_move[:, j + 10])
+            trial_stack.append(np.array(spkcounts)[:, j : j + 10])
+            trial_motion.append(np.array(kin_move)[:, j + 10])
+        
             
+        if i == 0:
+            stacked_bins = np.array(trial_stack)
+            motion_labels = np.array(trial_motion)
+        else:
+            stacked_bins = np.vstack((stacked_bins, np.array(trial_stack)))
+            motion_labels = np.vstack((motion_labels, np.array(trial_motion)))
+            
+    stacked_bins = np.transpose(stacked_bins.astype('float32'), axes = [0, 2, 1])
+    motion_labels = motion_labels.astype('float32')
     
-        stacked_bins.append(trial_stack)
-        motion_labels.append(trial_motion)
+    #print(stacked_bins.shape)
         
     return stacked_bins, motion_labels
-                 
+
 
 #monkey1 = 'COS071212_MOCAP.mat'
 #monkey2 = 'GAR080710_MOCAP.mat'
     
-fr, km = align_kin('COS071212_MOCAP.mat')
+#monkey = loadmat(monkey1)
+
+        
+    
